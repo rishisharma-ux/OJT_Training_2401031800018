33c1300cd0c15290ef6b55db4c1f4eb7a8eff0f2
rishisharma-ux <rishisharma132007@gmail.com>
2026-06-03 23:08:36 +0530

    Add initial Content component in Content.js

---
+++ Day_15/Content.js
@@ -0,0 +1,11 @@
+
+function Content() {
+    return (
+        <div>
+            <h2>Main Content</h2>
+            <p>This is my first React Component.</p>
+        </div>
+    );
+}
+
+export default Content;
