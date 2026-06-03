422517f878aafb5ae49e38ee569d0d85c61fa6d7
rishisharma-ux <rishisharma132007@gmail.com>
2026-06-03 23:09:55 +0530

    Initialize App component with layout structure

---
+++ Day_15/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Content from './components/Content';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
